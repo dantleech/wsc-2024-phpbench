9ceef8e1c191edbb246c1f4a3ea66eb131d6f277
Daniel Leech <daniel@dantleech.com>
2024-07-03 06:55:32 +0100

    Part 2

--- tests/Benchmark/LexerBench.php
+++ tests/Benchmark/LexerBench.php
@@ -2,13 +2,37 @@
 
 namespace WSC\Tests\Benchmark;
 
+use Generator;
+use PhpBench\Attributes\ParamProviders;
 use WSC\Parser\Lexer;
+use PhpBench\Attributes as Bench;
 
+#[Bench\Iterations(10)]
+#[Bench\Revs(10)]
+#[Bench\BeforeMethods('setUp')]
 final class LexerBench
 {
-    public function benchLexer(): void
+    private Lexer $lexer;
+
+    public function setUp(): void
     {
-        $lexer = new Lexer();
-        $lexer->lex('+ 1 2');
+        $this->lexer = new Lexer();
+    }
+
+    #[Bench\ParamProviders('provideLexer')]
+    #[Bench\Assert('mode(variant.time.avg) < 1 microsecond +/- 10%')]
+    public function benchLexer(array $params): void
+    {
+        $this->lexer->lex($params['expr']);
+    }
+
+    public function provideLexer(): Generator
+    {
+        yield 'one plus one' => [
+            'expr' => '+ 1 2',
+        ];
+        yield 'long expression' => [
+            'expr' => '+ 2 2 * 2 4 / 1 + 5 * 10 10',
+        ];
     }
 }
